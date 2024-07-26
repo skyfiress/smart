bf2d9c3b88fbb9e6880a8dad84dc28f570e0a5bb
yang <136062372@qq.com>
2024-07-26 17:53:37 +0800

    1.0.7 升级新增implementation地址提示

---
+++ scripts/test.js
@@ -0,0 +1,11 @@
+// scripts/upgrade-box.js
+const { ethers, upgrades } = require("hardhat");
+
+async function main() {
+ 
+  const BOX_ADDRESS = "0xaF96fb3CE523B1A18369cdf31D86b3BEAC938Ba5";
+  const currentImplAddress = await upgrades.erc1967.getImplementationAddress(BOX_ADDRESS);
+  console.log("Smartp implementation Address", currentImplAddress);
+}
+
+main();
